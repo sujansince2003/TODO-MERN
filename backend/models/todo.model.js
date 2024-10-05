f8f82eebbf9c3b85397333d9503543f12b003ec2
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-10-05 21:35:06 +0545

    defined model for todo

---
+++ backend/models/todo.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Todo = mongoose.model("todo", todoSchema);
+model.exports = Todo;
