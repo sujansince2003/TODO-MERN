1f600c82f616510fe8b694e79c5c30b9f265ca6e
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-10-05 21:34:53 +0545

    defined middleware to handle authentiction with Jwt token based auth

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+async function auth(req, res, next) {
+  const token = req.header.authorization;
+  if (!token) {
+    res.status(403).json({
+      message: "Token not provided",
+    });
+  }
+  try {
+    const decodedUserData = await jwt.verify(
+      token.split(" ")[1],
+      process.env.JWT_SECRET_KEY
+    );
+    req.userData = decodedUserData;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "invalid Token",
+    });
+  }
+}
+
+module.exports = auth;
