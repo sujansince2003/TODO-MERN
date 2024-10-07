ffbaae6e7e224c3b8d94f1293ad0b8f5dee72275
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-10-07 09:52:26 +0545

    setup CORS for connecting to frontned

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("../backend/routes/user.route");
 const todoRoutes = require("../backend/routes/todo.route");
+const cors = require("cors");
 
 // configuring dotenv
 dotenv.config({});
@@ -11,6 +12,17 @@ dotenv.config({});
 const app = express();
 app.use(express.json());
 
+//setting up cors
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
+app.options("*", cors());
 //routes
 app.use("/api/user", userRoutes);
 app.use("/api/todo", todoRoutes);
@@ -18,6 +30,6 @@ app.use("/api/todo", todoRoutes);
 //connnect to database
 connectDB();
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("server running");
 });
